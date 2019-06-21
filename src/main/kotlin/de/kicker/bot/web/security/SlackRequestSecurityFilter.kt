5821ed8688ee8f5f5a908848c874746278719ab8
Tuan Pham <tuan.pham@itemis.de>
2019-06-21 16:15:47 +0200

    use verifier for the slack filter WIP

--- src/main/kotlin/de/kicker/bot/web/security/SlackRequestSecurityFilter.kt
+++ src/main/kotlin/de/kicker/bot/web/security/SlackRequestSecurityFilter.kt
@@ -1,20 +1,28 @@
 package de.kicker.bot.web.security
 
 import org.springframework.stereotype.Component
-import org.springframework.web.filter.GenericFilterBean
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
-import javax.servlet.ServletRequest
-import javax.servlet.ServletResponse
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Component
-class SlackRequestSecurityFilter : OncePerRequestFilter() {
+class SlackRequestSecurityFilter constructor(val verifier: SlackCommandRequestVerifier) : OncePerRequestFilter() {
 
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
-        logger.info("SlackRequest")
-        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied")
+        logger.info("Check SlackRequest")
+        val timestamp = request.getHeader("x-slack-request-timestamp") ?: ""
+        val slackSignature = request.getHeader("x-slack-signature") ?: ""
+        val requestBody = getBody(request)
+        val verifierResult = verifier.verifySlackSignature(slackSignature, requestBody, timestamp)
+        if (!verifierResult) {
+            logger.info("SlackRequest is not valid")
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied")
+        } else {
+            logger.info("SlackRequest is valid")
+        }
     }
+
+    private fun getBody(req: HttpServletRequest): String = req.inputStream.bufferedReader().use { it.readText() }
 
 }
