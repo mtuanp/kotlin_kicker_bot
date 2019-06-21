15814dc28dcee38b9c086f21d5df2ce1c19e2524
Tuan Pham <tuan.pham@itemis.de>
2019-06-21 14:59:33 +0200

    add slack request verifier and unit test

--- src/main/kotlin/de/kicker/bot/web/security/SlackCommandRequestVerifier.kt
+++ src/main/kotlin/de/kicker/bot/web/security/SlackCommandRequestVerifier.kt
@@ -2,15 +2,35 @@ package de.kicker.bot.web.security
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import java.nio.charset.Charset
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 
+
+
+/**
+ * CLass for verified the slack request. It create a signature from the request body, timestamp and version number and check against the given signature.
+ */
 @Component
-class SlackCommandRequestVerifier {
+class SlackCommandRequestVerifier(@Value("\${slack.signature.key}") private val slackSignatureKey: String) {
 
-    @Value("\${slack.signature.key}")
-    private lateinit var slackSignatureKey: String
+    private val hashAlgorithm = "HmacSHA256"
 
-    fun verifySlackSignature(requestBody: String, timestamp: String, versionNumber : String = "v0") : Boolean {
-        return false
+    /**
+     * Create a signature from the request body, timestamp and version number and check against the given signature. True if the given signature is equals the generated signature.
+     */
+    fun verifySlackSignature(givenSignature: String, requestBody: String, timestamp: String, versionNumber: String = "v0"): Boolean {
+        val signatureContent = "$versionNumber:$timestamp:$requestBody"
+        val signatureWithoutVersion = givenSignature.removePrefix("$versionNumber=")
+        val generatedSignature = generateSignature(signatureContent)
+        return signatureWithoutVersion == generatedSignature
+    }
+    
+    fun generateSignature(signatureContent: String) : String {
+        val hasher = Mac.getInstance(hashAlgorithm)
+        hasher.init(SecretKeySpec(slackSignatureKey.toByteArray(), hashAlgorithm))
+        val macResult = hasher.doFinal(signatureContent.toByteArray())
+        return macResult.fold(StringBuilder(), { builder, it -> builder.append("%02x".format(it)) }).toString().toLowerCase()
     }
 
 }
