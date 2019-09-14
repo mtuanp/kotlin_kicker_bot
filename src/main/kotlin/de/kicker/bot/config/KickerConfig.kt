96dbc2f2b358d163db2bb35b014c13eb40d80966
Tuan Pham <7u4n.pm@gmail.com>
2019-09-14 23:14:28 +0200

    add logging for cache settings

--- src/main/kotlin/de/kicker/bot/config/KickerConfig.kt
+++ src/main/kotlin/de/kicker/bot/config/KickerConfig.kt
@@ -3,6 +3,8 @@ package de.kicker.bot.config
 import com.google.common.cache.Cache
 import com.google.common.cache.CacheBuilder
 import de.kicker.bot.api.KickerMatch
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -10,10 +12,12 @@ import java.util.concurrent.TimeUnit
 
 @Configuration
 class KickerConfig {
+    val logger: Logger = LoggerFactory.getLogger(KickerConfig::class.java)
 
     @Bean
     @Profile("!dev")
     fun kickerMatchCacheProd(): Cache<String, KickerMatch> {
+        logger.info("load production cache settings")
         return CacheBuilder.newBuilder()
                 .maximumSize(1000)
                 .expireAfterWrite(1, TimeUnit.HOURS)
@@ -23,12 +27,11 @@ class KickerConfig {
     @Bean
     @Profile("dev")
     fun kickerMatchCacheDev(): Cache<String, KickerMatch> {
+        logger.info("load dev cache settings")
         return CacheBuilder.newBuilder()
                 .maximumSize(10)
                 .expireAfterAccess(30, TimeUnit.SECONDS)
                 .build();
     }
-
-
 
 }
